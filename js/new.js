ccb414e7dc5c4f36ab2fa80340ac89183e005328
Anzor <anzor.is95@gmail.com>
2023-03-18 14:41:57 +0300

    first commit

---
+++ js/new.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/* alert("Hello"); */
+
+/* const result = confirm("Are you here?");
+console. log(result);
+ */
+
+const answer = +prompt("Вам есть 18?", "");
+console.log(answer + 10);
